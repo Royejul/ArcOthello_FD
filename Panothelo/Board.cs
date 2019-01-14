799da57a8041db30d3cff90db3100b569d386fbb
julien.feuillad <julien.feuillade@he-arc.ch>
2019-01-14 16:31:51 +0100

    Remise à niveau correcte des fichiers

---
+++ Panothelo/Board.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panothelo
+{
+    class Board : IPlayable.IPlayable
+    {
+        //Attributs
+        private int[,] matBoard;
+        private int nbCol;
+        private int nbLin;
+
+        public Board(int col, int line)
+        {
+            this.nbCol = col;
+            this.nbLin = line;
+            matBoard = new int[col, line];
+            initBoard();
+        }
+
+        public int GetBlackScore()
+        {
+            int score = 0;
+            for (int i=0; i<nbCol;i++)
+            {
+                for(int j=0;i<nbLin;j++)
+                {
+                    if (matBoard[i, j] == 1)
+                        score++;
+                }
+            }
+            return score;
+        }
+
+        public int[,] GetBoard()
+        {
+            return matBoard;
+        }
+
+        public string GetName()
+        {
+            return "IAName";
+        }
+
+        public Tuple<int, int> GetNextMove(int[,] game, int level, bool whiteTurn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetWhiteScore()
+        {
+            int score = 0;
+            for (int i = 0; i < nbCol; i++)
+            {
+                for (int j = 0; i < nbLin; j++)
+                {
+                    if (matBoard[i, j] == 0)
+                        score++;
+                }
+            }
+            return score;
+        }
+
+        public bool IsPlayable(int column, int line, bool isWhite)
+        {
+            if (checkLeft(column, line, isWhite))
+                return true;
+            if (checkRight(column, line, isWhite))
+                return true;
+            if (checkTop(column, line, isWhite))
+                return true;
+            if (checkBottom(column, line, isWhite))
+                return true;
+            if (checkDLT(column, line, isWhite))
+                return true;
+            if (checkDLB(column, line, isWhite))
+                return true;
+            if (checkDRT(column, line, isWhite))
+                return true;
+            if (checkDRB(column, line, isWhite))
+                return true;
+            return false;
+        }
+
+        public bool PlayMove(int column, int line, bool isWhite)
+        {
+            int tok = 1;
+            bool allOK = false;
+            if (isWhite)
+                tok = 0;
+            if (checkLeft(column, line, isWhite))
+            {
+                swapToken(column - 1, line, -1, 0, tok);
+                allOK = true;
+            }
+            if (checkRight(column, line, isWhite))
+            {
+                swapToken(column + 1, line, 1, 0, tok);
+                allOK = true;
+            }
+            if (checkTop(column, line, isWhite))
+            {
+                swapToken(column, line - 1, 0, -1, tok);
+                allOK = true;
+            }
+            if (checkBottom(column, line, isWhite))
+            {
+                swapToken(column, line + 1, 0, 1, tok);
+                allOK = true;
+            }
+            if (checkDLT(column, line, isWhite))
+            {
+                swapToken(column + 1, line - 1, -1, -1, tok);
+                allOK = true;
+            }
+            if (checkDLB(column, line, isWhite))
+            {
+                swapToken(column - 1, line + 1, -1, 1, tok);
+                allOK = true;
+            }
+            if (checkDRT(column, line, isWhite))
+            {
+                swapToken(column + 1, line - 1, 1, -1, tok);
+                allOK = true;
+            }
+            if (checkDRB(column, line, isWhite))
+            {
+                swapToken(column + 1, line + 1, -1, 1, tok);
+                allOK = true;
+            }
+            if(allOK)
+            {
+                matBoard[column, line] = tok;
+                return allOK;
+            }
+            return allOK;
+        }
+
+        // privates methods
+
+        private void initBoard()
+        {
+            int condI = nbCol / 2;
+            int condJ = nbLin / 2;
+            for (int i=0; i<nbCol;i++)
+            {
+                for(int j=0; j<nbLin;j++)
+                {
+                    if ((i==condI && j==condJ)||(i == condI+1 && j == condJ+1))
+                        matBoard[i, j] = 0;
+                    else if ((i == condI+1 && j == condJ) || (i == condI && j == condJ + 1))
+                        matBoard[i, j] = 1;
+                    else
+                        matBoard[i, j] = -1;
+                }
+            }
+        }
+
+        private void swapToken(int col, int line, int stepI, int stepJ, int stopTok)
+        {
+            if (stopTok != matBoard[col, line])
+            {
+                matBoard[col, line] = stopTok;
+                swapToken(col + stepI, line + stepJ, stepI, stepJ, stopTok);
+            }
+        }
+
+        private bool checkLeft(int column, int line, bool isWhite)
+        {
+            int tokenS = 0;
+            int tokenE = 1;
+            bool okMove = false;
+            if (isWhite)
+            {
+                tokenS = 1;
+                tokenE = 0;
+            }
+            for (int i = column - 1; i > -1; i--)
+            {
+                if (matBoard[i, line] == tokenS && !okMove)
+                {
+                    okMove = true;
+                }
+                else if (matBoard[i, line] == tokenE && okMove)
+                    return true;
+            }
+            return false;
+        }
+        private bool checkRight(int column, int line, bool isWhite)
+        {
+            int tokenS = 0;
+            int tokenE = 1;
+            bool okMove = false;
+            if (isWhite)
+            {
+                tokenS = 1;
+                tokenE = 0;
+            }
+            for (int i = column + 1; i > nbCol; i++)
+            {
+                if (matBoard[i, line] == tokenS && !okMove)
+                {
+                    okMove = true;
+                }
+                else if (matBoard[i, line] == tokenE && okMove)
+                    return true;
+            }
+            return false;
+        }
+        private bool checkTop(int column, int line, bool isWhite)
+        {
+            int tokenS = 0;
+            int tokenE = 1;
+            bool okMove = false;
+            if (isWhite)
+            {
+                tokenS = 1;
+                tokenE = 0;
+            }
+            for (int j = line - 1; j > -1; j--)
+            {
+                if (matBoard[column, j] == tokenS && !okMove)
+                {
+                    okMove = true;
+                }
+                else if (matBoard[column, j] == tokenE && okMove)
+                    return true;
+            }
+            return false;
+        }
+        private bool checkBottom(int column, int line, bool isWhite)
+        {
+            int tokenS = 0;
+            int tokenE = 1;
+            bool okMove = false;
+            if (isWhite)
+            {
+                tokenS = 1;
+                tokenE = 0;
+            }
+            for (int j = line + 1; j > nbLin; j++)
+            {
+                if (matBoard[column, j] == tokenS && !okMove)
+                {
+                    okMove = true;
+                }
+                else if (matBoard[column, j] == tokenE && okMove)
+                    return true;
+            }
+            return false;
+        }
+        private bool checkDLT(int column, int line, bool isWhite)
+        {
+            int tokenS = 0;
+            int tokenE = 1;
+            bool okMove = false;
+            if (isWhite)
+            {
+                tokenS = 1;
+                tokenE = 0;
+            }
+            for (int i = column - 1; i > -1; i--)
+            {
+                for (int j = line - 1; j > -1; j--)
+                {
+                    if (matBoard[i, j] == tokenS && !okMove)
+                    {
+                        okMove = true;
+                    }
+                    else if (matBoard[i, j] == tokenE && okMove)
+                        return true;
+                }
+            }
+            return false;
+        }
+        private bool checkDLB(int column, int line, bool isWhite)
+        {
+            int tokenS = 0;
+            int tokenE = 1;
+            bool okMove = false;
+            if (isWhite)
+            {
+                tokenS = 1;
+                tokenE = 0;
+            }
+            for (int i = column - 1; i > -1; i--)
+            {
+                for (int j = line + 1; j > nbLin; j++)
+                {
+                    if (matBoard[i, j] == tokenS && !okMove)
+                    {
+                        okMove = true;
+                    }
+                    else if (matBoard[i, j] == tokenE && okMove)
+                        return true;
+                }
+            }
+            return false;
+        }
+        private bool checkDRT(int column, int line, bool isWhite)
+        {
+            int tokenS = 0;
+            int tokenE = 1;
+            bool okMove = false;
+            if (isWhite)
+            {
+                tokenS = 1;
+                tokenE = 0;
+            }
+            for (int i = column + 1; i > nbCol; i++)
+            {
+                for (int j = line - 1; j > -1; j--)
+                {
+                    if (matBoard[i, j] == tokenS && !okMove)
+                    {
+                        okMove = true;
+                    }
+                    else if (matBoard[i, j] == tokenE && okMove)
+                        return true;
+                }
+            }
+            return false;
+        }
+        private bool checkDRB(int column, int line, bool isWhite)
+        {
+            int tokenS = 0;
+            int tokenE = 1;
+            bool okMove = false;
+            if (isWhite)
+            {
+                tokenS = 1;
+                tokenE = 0;
+            }
+            for (int i = column + 1; i > nbCol; i++)
+            {
+                for (int j = line + 1; j > nbLin; j++)
+                {
+                    if (matBoard[i, j] == tokenS && !okMove)
+                    {
+                        okMove = true;
+                    }
+                    else if (matBoard[i, j] == tokenE && okMove)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Panothelo/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+using System.Timers;
+using System.Windows.Threading;
+using System.ComponentModel;
+using System.Xml;
+
+namespace Panothelo
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        int gridColumn = 9;
+        int gridRow = 7;
+
+        ImageBrush blackPawn;
+        ImageBrush whitePawn;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGame();
+            InitializeBoard();
+
+
+        }
+
+        private void InitializeGame()
+        {
+            blackPawn = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Panothelo;component/PawnImage/BlackPawn.png")));
+            whitePawn = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Panothelo;component/PawnImage/WhitePawn.png")));
+
+        }
+
+        private void InitializeBoard()
+        {
+            for (int i = 0; i < gridColumn; i++)
+            {
+                for (int j = 0; j < gridRow; j++)
+                {
+                    Label lblGrid = new Label
+                    {
+                        BorderBrush = Brushes.Black,
+                        BorderThickness = new Thickness(2)
+                    };
+
+                    lblGrid.MouseEnter += MouseEnterGrid;
+                    lblGrid.MouseLeave += MouseLeaveGrid;
+                    lblGrid.MouseLeftButtonUp += MouseButtonUpGrid;
+
+                    Grid.SetRow(lblGrid, j);
+                    Grid.SetColumn(lblGrid, i);
+                    Board.Children.Add(lblGrid);
+                }
+            }
+        }
+
+        private void MouseEnterGrid(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Label lblGrid = sender as Label;
+        }
+
+        private void MouseLeaveGrid(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Label lblGrid = sender as Label;
+        }
+
+        private void MouseButtonUpGrid(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Label lblGrid = sender as Label;
+            int col = Grid.GetColumn(lblGrid);
+            int row = Grid.GetRow(lblGrid);
+
+            //MessageBox.Show(col.ToString() + " - " + row.ToString());
+
+            lblGrid.Background = whitePawn;
+
+
+
+
+        }
+
+    }
+
+}
